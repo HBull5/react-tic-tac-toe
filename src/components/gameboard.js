7a820b1773167126e644497f29c9e31364d091cc
HBull5 <harcourtbullv@gmail.com>
2022-07-03 22:53:22 -0400

    styles in place for tic tac toe board

---
+++ src/components/gameboard.js
@@ -0,0 +1,17 @@
+function GameBoard() {
+	return (
+        <div class="gameboard">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    )
+}
+
+export default GameBoard;
