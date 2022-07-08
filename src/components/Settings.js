d688ae7875bc80abfc5a8ed36ba81abbc2e149c6
HBull5 <harcourtbullv@gmail.com>
2022-07-07 22:24:06 -0400

    adjusted a few things

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -2,14 +2,19 @@ import { useRef } from 'react';
 import Popup from './Popup';
 
 function Settings() {
-    const popupRef = useRef(); 
+	const popupRef = useRef();
 
-    return(
-        <div className="settings">
-            <i className="fas fa-cog" onClick={() => {popupRef.current.classList.remove('hidden')}}></i>
-            <Popup ref={popupRef} type='settings' />
-        </div>
-    );
+	return (
+		<div className="settings">
+			<i
+				className="menu fas fa-cog"
+				onClick={() => {
+					popupRef.current.classList.remove('hidden');
+				}}
+			></i>
+			<Popup ref={popupRef} type="settings" />
+		</div>
+	);
 }
 
-export default Settings; 
+export default Settings;
